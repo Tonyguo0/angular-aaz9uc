97f9afc27d89a551844d5c343f0d366798241b60
Tonyguo0 <46705969+Tonyguo0@users.noreply.github.com>
2023-04-17 18:24:45 +0800

    3 files modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-
+  name = new FormControl();
 }
