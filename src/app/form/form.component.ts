fee3992972a0bfd9c70eba4975c42b9218d0834f
Tonyguo0 <46705969+Tonyguo0@users.noreply.github.com>
2023-05-01 23:41:57 +0800

    added formGroup, nestedFormGroup, formBuilder Service, dynamic form addition,

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,5 +13,56 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  name = new FormControl();
+  profileForm2 = this.fb.group({
+    fname: ['', Validators.required],
+    lname: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.fb.array([this.fb.control('')]),
+  });
+  constructor(private fb: FormBuilder) {}
+
+  fname = new FormControl();
+  lname = new FormControl();
+
+  profileForm = new FormGroup({
+    fname: this.fname,
+    lname: this.lname,
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      zip: new FormControl(),
+    }),
+  });
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
+  disableSubmitButton() {
+    return this.profileForm.valid;
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      fname: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+
+  // we need ot get the aliases ArrayForm element from the profileForm instance
+  get aliases() {
+    return this.profileForm2.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
 }
