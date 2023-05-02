31c7219c51965e8e245630cbcc1dd87fdf4e2d37
Tonyguo0 <46705969+Tonyguo0@users.noreply.github.com>
2023-05-02 12:46:10 +0800

    added dynamic formgroup control within formarray control

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,6 +13,7 @@ import {
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
+  // FormBuilder service created
   profileForm2 = this.fb.group({
     fname: ['', Validators.required],
     lname: [''],
@@ -22,47 +23,56 @@ export class FormComponent {
       state: [''],
       zip: [''],
     }),
-    aliases: this.fb.array([this.fb.control('')]),
+    aliases: this.fb.array([
+      this.fb.group({ alias1: [''], parent1: [''], parent2: [''] }),
+    ]),
   });
   constructor(private fb: FormBuilder) {}
 
-  fname = new FormControl();
-  lname = new FormControl();
+  // fname = new FormControl();
+  // lname = new FormControl();
 
-  profileForm = new FormGroup({
-    fname: this.fname,
-    lname: this.lname,
-    address: new FormGroup({
-      street: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      zip: new FormControl(),
-    }),
-  });
+  // profileForm = new FormGroup({
+  //   fname: this.fname,
+  //   lname: this.lname,
+  //   address: new FormGroup({
+  //     street: new FormControl(),
+  //     city: new FormControl(),
+  //     state: new FormControl(),
+  //     zip: new FormControl(),
+  //   }),
+  // });
 
   onSubmit() {
-    console.warn(this.profileForm.value);
+    console.warn(this.profileForm2.value);
   }
 
   disableSubmitButton() {
-    return this.profileForm.valid;
+    return this.profileForm2.valid;
   }
 
   updateProfile() {
-    this.profileForm.patchValue({
+    this.profileForm2.patchValue({
       fname: 'Nancy',
       address: {
         street: '123 Drew Street',
       },
+      aliases: [{ alias1: 'hello', parent1: 'dad', parent2: 'mom' }],
     });
   }
 
-  // we need ot get the aliases ArrayForm element from the profileForm instance
+  // we need ot get the aliases ArrayForm element from the profileForm2 instance made from buildForm Service
   get aliases() {
     return this.profileForm2.get('aliases') as FormArray;
   }
 
   addAlias() {
-    this.aliases.push(this.fb.control(''));
+    this.aliases.push(
+      this.fb.group({
+        alias1: [''],
+        parent1: [''],
+        parent2: [''],
+      })
+    );
   }
 }
